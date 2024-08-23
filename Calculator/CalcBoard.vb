7759a7736e08d41449c1dac6b616a60888ced847
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-23 05:36:25 +0330

    - Update Clculator User Interface

--- Calculator/CalcBoard.vb
+++ Calculator/CalcBoard.vb
@@ -1,3 +1,20 @@
 ﻿Module CalcBoard
-
+    Public Sub Draw()
+        Console.WriteLine("   ╔═══════════╗")
+        Console.WriteLine("   ║           ║")
+        Console.WriteLine("   ║═══════════║")
+        Console.WriteLine("   ║           ║")
+        Console.WriteLine("   ║═══════════║")
+        Console.WriteLine("   ║  ║    ║   ║")
+        Console.WriteLine("   ║═══╦═══╦═══║")
+        Console.WriteLine("   ║ 7 ║ 8 ║ 9 ║")
+        Console.WriteLine("   ╠═══╬═══╬═══╣")
+        Console.WriteLine("   ║ 4 ║ 5 ║ 6 ║")
+        Console.WriteLine("   ╠═══╬═══╬═══╣")
+        Console.WriteLine("   ║ 1 ║ 2 ║ 3 ║")
+        Console.WriteLine("   ╠═══╬═══╬═══╣")
+        Console.WriteLine("   ║ ¬ ║ 0 ║ . ║")
+        Console.WriteLine("   ╚═══╩═══╩═══╝")
+        Console.WriteLine()
+    End Sub
 End Module
