92c5254a957196a09eb47e24bbcd8dfd2969f856
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-25 05:29:07 +0330

    - Create ReadCharacterAt Module (Get the character that the console Cursor is on)

--- Calculator/Operations.vb
+++ Calculator/Operations.vb
@@ -1,4 +1,6 @@
-﻿Module Operations
+﻿Imports System.Runtime.InteropServices
+
+Module Operations
     Enum OperationStatus
         None
         Plus
@@ -6,10 +8,24 @@
         Divide
         Multiply
     End Enum
+
+
+    <DllImport("kernel32.dll", CharSet:=CharSet.Unicode, SetLastError:=True)>
+    Private Function GetConsoleScreenBufferInfo(consoleHandle As IntPtr, <Out> ByRef consoleScreenBufferInfo As CONSOLE_SCREEN_) As Boolean
+    End Function
+
     Public Property Status As Calculator.Operations.OperationStatus = OperationStatus.None
     Public Sub Plus()
+        Dim oneNumber As String = "0"
         Calculator.Operations.Status = OperationStatus.Plus
-
+        System.Console.SetCursorPosition(5, 3)
+        cursor = 5
+        For index = 5 To 12
+            cursor += 1
+            System.Console.SetCursorPosition(cursor, 3)
+            oneNumber = Console.inpu
+        Next
+        Calculator.Program.OneNumber = 
     End Sub
 
     Public Sub Minus()
---
+++ Calculator/ReadCharacterAt.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Text
+
+Module ReadCharacterAt
+    <DllImport("kernel32.dll", SetLastError:=True)>
+    Private Function GetStdHandle(nStdHandle As Integer) As IntPtr
+    End Function
+
+    <DllImport("kernel32.dll", SetLastError:=True)>
+    Private Function ReadConsoleOutputCharacter(hConsoleOutput As IntPtr,
+                    <Out> lpCharacter As StringBuilder, length As UInteger, bufferCoord As COORD, <Out> ByRef lpNumberOfCharactersRead As UInteger) As Boolean
+
+
+    End Function
+
+    <StructLayout(LayoutKind.Sequential)>
+    Public Structure COORD
+        Public X As Short
+        Public Y As Short
+    End Structure
+
+    Public Function ReadCharacterAt(x As Integer, y As Integer) As Char
+        Dim consoleHandle As IntPtr = GetStdHandle(-11)
+        If consoleHandle = IntPtr.Zero Then
+            Return ChrW(0)
+        End If
+        Dim position As COORD = New COORD With {
+        .X = x,
+        .Y = y
+    }
+        Dim result As StringBuilder = New StringBuilder(1)
+        Dim read As UInteger = 0
+        If ReadConsoleOutputCharacter(consoleHandle, result, 1, position, read) Then
+            Return result(0)
+        Else
+            Return ChrW(0)
+        End If
+    End Function
+
+End Module
